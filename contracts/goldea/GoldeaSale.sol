d3bb7edf7b7b7599f447d539a7131b6128fec488
goldea <goldea.goldea@gmail.com>
2017-11-13 10:00:02 +0300

    renamed Sale to PreSale added common AbstractSale (sells anything) added Sale (sells tokens) added GoldeaSale (sells tokens and changes ETH and BTC rates at once)
    fixed warnings

--- contracts/goldea/GoldeaSale.sol
+++ contracts/goldea/GoldeaSale.sol
@@ -4,12 +4,6 @@ pragma solidity ^0.4.15;
 import "../sale/Sale.sol";
 
 
-/**
- * 1. Цены - Считаются так: Базовая цена. Есть 4 базовые цены с началом дат. 12, 15, 18, 21
- *    для получения цены по токену нужно взять базовую цену, с учетом курса
- * 2. Бонусы
- * @dev Basic version of StandardToken, with no allowances.
- */
 contract GoldeaSale is Sale {
     address btcToken;
 
