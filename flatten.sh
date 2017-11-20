37351f69a04ae46b7805f3a04f9ffb3e987ed25a
goldea <goldea.goldea@gmail.com>
2017-11-20 17:58:39 +0300

    flatten GoldeaBounty

--- flatten.sh
+++ flatten.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 solidity_flattener --solc-paths="..=.." --output GoldeaSale.sol contracts/goldea/GoldeaSale.sol
+solidity_flattener --solc-paths="..=.." --output GoldeaBounty.sol contracts/goldea/GoldeaBounty.sol
